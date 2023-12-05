a3b30e895662cc514b4e8e58fd164d6adc50dbdd
dyadyasawa <dyadyasawa@gmail.com>
2023-12-05 08:48:43 +0300

    Добавление функции селекции списка словарей в functions/utils.py.

--- functions/utils.py
+++ functions/utils.py
@@ -7,3 +7,15 @@ def load_data(load_path):
     with open(load_path, "r", encoding="UTF8") as file:
         list_dict = json.load(file)
     return list_dict
+
+
+def selection_list(list_dict):
+    """Создает новый список словарей, удаляя пустые, и отбирая по заданному значению из входящего """
+
+    result_list = []
+    for item in list_dict:
+        if item == {}:
+            continue
+        elif item['state'] == 'EXECUTED':
+            result_list.append(item)
+    return result_list
