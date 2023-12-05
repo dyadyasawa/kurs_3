ebaf806f2d9b2af785823afdb2ad186e3a833907
dyadyasawa <dyadyasawa@gmail.com>
2023-12-05 09:02:03 +0300

    Добавление теста функции селекции в tests/test_utils.py, а также фикстуры для него в tests/conftest.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,8 @@ from config import LOAD_DATA_PATH
 @pytest.fixture()
 def start_list():
     return utils.load_data(f"../{LOAD_DATA_PATH}")
+
+
+@pytest.fixture()
+def blank_list_dict():
+    return [{}, {'state': 'CANCELED', 'name': 'noname'}, {'state': 'EXECUTED'}]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,3 +6,7 @@ from functions import utils
 def test_load_data(start_list):
     assert type(start_list) == list
     assert type(start_list[randint(0, len(start_list))]) == dict
+
+
+def test_selection_list(blank_list_dict):
+    assert len(utils.selection_list(blank_list_dict)) == 1
